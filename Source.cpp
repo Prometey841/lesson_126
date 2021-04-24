02c1b72af9b814f6ecd770f3d47a4ec199fec806
Дмитрий Лоскутников <ldb.84@mail.ru>
2021-04-25 01:03:55 +0700

    Разбивка на файлы

--- Source.cpp
+++ Source.cpp
@@ -1,52 +1,5 @@
-#include <iostream>
-
-class Vector3D;
-
-class Point3D
-{
-private:
-	double m_x, m_y, m_z;
-
-public:
-	Point3D(double x = 0.0, double y = 0.0, double z = 0.0)
-		: m_x(x), m_y(y), m_z(z)
-	{
-
-	}
-
-	void print()
-	{
-		std::cout << "Point(" << m_x << " , " << m_y << " , " << m_z << ")\n";
-	}
-
-	void moveByVector(const Vector3D& v);
-};
-
-class Vector3D
-{
-private:
-	double m_x, m_y, m_z;
-
-public:
-	Vector3D(double x = 0.0, double y = 0.0, double z = 0.0)
-		: m_x(x), m_y(y), m_z(z)
-	{
-	}
-
-	void print()
-	{
-		std::cout << "Vector(" << m_x << " , " << m_y << " , " << m_z << ")\n";
-	}
-
-	friend void Point3D::moveByVector(const Vector3D& v);
-};
-
-void Point3D::moveByVector(const Vector3D& v)
-{
-	m_x += v.m_x;
-	m_y += v.m_y;
-	m_z += v.m_z;
-}
+#include "Point3D.h"
+#include "Vector3D.h"
 
 int main()
 {
